62399ada75b030fc3034f713030bbba3cc2de730
syd333 <sydmiranda@icloud.com>
2021-03-02 13:43:56 -0800

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 6.2 
+const num2 = 10
+let multiply = num1 * num2
+
+let random =  Math.floor(Math.random() * 10)
+
+
+const num3 = 14, num4 = 5
+const mod = num3 % num4
+
+const max = Math.max(20, 10)
